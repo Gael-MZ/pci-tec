cd422340d944c8ac3abd5db5f1c792ae4bcc86d1
Gael-MZ <a01713218@tec.mx>
2024-08-18 17:56:03 -0600

    Add files via upload

--- Programación/main.py
+++ Programación/main.py
@@ -1,7 +1,18 @@
-#Gael
-'''
-Comentario
-'''
-print("\nTecnológico de Monterrey\n")
-print("Bienvenidos al curso de PCI\n")
-print("\tAtentamente: Gael\n")
+print("\n Proceso para calcular el factorial de un número:\n")
+
+print("1. Solicitar el número:")
+print("\t 1.1- Pide al usuario que ingrese un número entero positivo")
+print("\t 1.2- Este número será el valor del factorial que queremos calcular\n")
+
+print("2. Inicializar el valor del factorial:")
+print("\t 2.1- Comenzamos con un valor de 1")
+print("\t 2.2- Este valor sirve para tenerlo de base para las próximas multiplicaciones\n")
+
+print("3. Calcular los números desde 1 hasta el número ingresado:")
+print("\t 3.1- Empezar con el número 1 y aumentar de uno en uno hasta llegar al número ingresado")
+print("\t 3.2- En cada paso, multiplica el valor actual del factorial por el número del recorrido")
+print("\t 3.3- Acumular la multiplicación hasta el número ingresado\n")
+
+print("4. Mostrar el resultado:")
+print("\t 4.1- Una vez completadas las multiplicaciones, el valor final es el factorial del número ingresado")
+print("\t 4.2- Mostramos este resultado al usuario\n")
